2e19a56e15303e93c6496d82dd9927b2b39e0608
Leonardo de Carvalho Nevado <75989546+LeonardoNevado@users.noreply.github.com>
2024-01-31 13:26:17 -0300

    New Producer/Consumer && Service Upgrade / Http Server Usage

--- service-fraud-detector/src/main/java/br/com/alura/ecommerce/Order.java
+++ service-fraud-detector/src/main/java/br/com/alura/ecommerce/Order.java
@@ -4,12 +4,30 @@ import java.math.BigDecimal;
 
 public class Order {
 
-    private final String userId, orderId;
+    private final String orderId;
     private final BigDecimal amount;
+    private final String email;
 
-    public Order(String userId, String orderId, BigDecimal amount) {
-        this.userId = userId;
+    public Order(String orderId, BigDecimal amount, String email) {
         this.orderId = orderId;
         this.amount = amount;
+        this.email = email;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "orderId='" + orderId + '\'' +
+                ", amount=" + amount +
+                ", email='" + email + '\'' +
+                '}';
     }
 }
