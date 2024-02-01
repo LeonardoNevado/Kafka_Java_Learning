48dc443bb793023410e3e657dadb9d98105ae278
Leonardo de Carvalho Nevado <75989546+LeonardoNevado@users.noreply.github.com>
2024-02-01 12:35:57 -0300

    fix

--- service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
+++ service-users/src/main/java/br/com/alura/ecommerce/CreateUserService.java
@@ -7,7 +7,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
 import java.util.UUID;
-import java.util.concurrent.ExecutionException;
 
 public class CreateUserService {
 
